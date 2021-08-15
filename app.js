cf20e79b71f71061510695ed9971aaebee77f39c
Sarah Almahmid <hpsarontm@gmail.com>
2021-08-15 18:36:00 +0300

    get tasks

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express();
 const db = require("./db/models/index");
 app.use(cors());
 
+//ROUTES
+const taskRoutes = require("./API/task/routes");
+app.use("/tasks", taskRoutes);
+
 const run = async () => {
   try {
-    await db.sequelize.sync({ force: true });
+    await db.sequelize.sync({ alter: true });
     console.log("Connection to the database successful!");
     app.listen(8000, () => {
       console.log("The application is running on localhost:8000");
