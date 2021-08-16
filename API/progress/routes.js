85e0a8d1a43ef73b71b85f83b3fe0e57f13729a4
AthariCoded <athari.rashed@gmail.com>
2021-08-16 19:52:22 +0300

    profile to progress

--- API/progress/routes.js
+++ API/progress/routes.js
@@ -2,40 +2,40 @@ const express = require("express");
 const passport = require("passport");
 
 const {
-  profileFetch,
-  fetchProfile,
-  fetchUserProfile,
-  updateProfile,
+  progressFetch,
+  fetchProgress,
+  fetchUserProgress,
+  updateProgress,
 } = require("./controllers");
 const router = express.Router();
 
 // param middleware (parameter)
-router.param("profileId", async (req, res, next, profileId) => {
-  // get the profile with id profileId
-  const profile = await fetchProfile(profileId, next);
-  if (profile) {
+router.param("progressId", async (req, res, next, progressId) => {
+  // get the progress with id progressId
+  const progress = await fetchProgress(progressId, next);
+  if (progress) {
     // store it in req
-    req.profile = profile;
+    req.progress = progress;
     next();
   } else {
     // give back response 404 Profile Not Found
-    const error = new Error("Profile Not Found.");
+    const error = new Error("Progress Not Found.");
     error.status = 404;
     next(error);
   }
 });
 
 // List Route
-router.get("/", profileFetch);
+router.get("/", progressFetch);
 
 //fetch profile
-router.get("/:profileId", fetchUserProfile);
+router.get("/:progressId", fetchUserProgress);
 
 // Update Route
 router.put(
-  "/:profileId",
+  "/:progressId",
   passport.authenticate("jwt", { session: false }),
-  updateProfile
+  updateProgress
 );
 
 module.exports = router;
